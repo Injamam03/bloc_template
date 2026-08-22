49816587b49958297b7b45d13e0ceae52c188286
injam <injamamulh544@gmail.com>
2026-08-22 12:48:38 +0600

    add new dart file  failure dart in core

--- lib/features/auth/domain/entities/user_entity.dart
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -1,1 +1,18 @@
-// User Entity
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String id;
+  final String name;
+  final String email;
+  final String token;
+
+  const UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.token,
+  });
+
+  @override
+  List<Object?> get props => [id, name, email, token];
+}
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,1 +1,16 @@
-// Auth Repository interface
+import 'package:dartz/dartz.dart';
+import 'package:bloc_template/core/error/failure.dart';
+import 'package:bloc_template/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserEntity>> login({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntity>> signup({
+    required String name,
+    required String email,
+    required String password,
+  });
+}
